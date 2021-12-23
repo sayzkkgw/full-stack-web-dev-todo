c79ddb92a404927129ff4156e36c8db255af50da
Seiji KK <sayzkkgw@gmail.com>
2021-12-23 10:06:41 +0000

    Develop HTML Structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,18 @@
+<div>
+    <!-- For CheckBox -->
+    <form action="" method="">
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark done/not done">Done/NotDone</button>
+    </form>
+
+    <!-- For TextField -->
+    <form action="" method="">
+        <input type="text" />
+        <button aria-label="Save todo">Save</button>
+    </form>
+    
+    <!-- For Delete -->
+    <form action="" method="">
+        <button aria-label="Delete todo">Delete</button>
+    </form>
+</div>
